fce0e5ade170ba2bcc4cd41947d218a7e8142a2c
Rahul Saini <145253788+Rahul86190@users.noreply.github.com>
2024-07-29 12:29:20 +0530

    surprise ( Moving Button).py

--- surprise ( Moving Button).py
+++ surprise ( Moving Button).py
@@ -11,18 +11,14 @@ def yes():
     l3.place(x=400,y=350)
     messagebox.showinfo("!!!!!   HURRRREYY   !!!!! ","DATE PROPOSAL ACCEPTED😁❤️    ^_^")
 
-
 def move_button(event):
     window_width=root.winfo_width()
     window_height=root.winfo_height()
-    
 
     new_x=random.randint(0,window_width - b2.winfo_width())
     new_y=random.randint(0,window_height - b2.winfo_height())
 
     b2.place(x=new_x,y=new_y)
-
-
 l1=tk.Label(text="DO YOU LIKE to DATE ME ❤️ ",font=("bold",30))
 # l1.grid(row=3,column=6)
 l1.place(x=400,y=100)
@@ -32,23 +28,4 @@ b1.place(x=500,y=200)
 b2=tk.Button(root,text="NO",fg="red")
 b2.place(x=500,y=250)
 b2.bind("<Enter>",move_button)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 root.mainloop()
