7844660dd36729103ff01e319e2ac01cf99cf371
Rahul Saini <145253788+Rahul86190@users.noreply.github.com>
2024-08-03 16:01:13 +0530

    EMAIL_SENDING.py

--- EMAIL_SENDING.py
+++ EMAIL_SENDING.py
@@ -6,11 +6,12 @@ import smtplib
 server=smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
 server.login('Sender_Email_address','password')      # put your mail at the place of 'Sender_Email_address' and receiver mail address at 'Receiver_Email_address;
-server.sendmail('Sender_Email_address','Receiver_Email_address',    
-                """SUBJECT:MAIL THROUGH PYTHON\n     
-                Namaste SIR,\n
-                NAME:RAHUL SAINI  \n
-                BRANCH:My Branch is -> ARTIFICIAL INTELLIGENCE AND DATA SCIENCE\n
-                COLLEGE:Arya College of Engineering\n
-                COLLEGE CITY:JAIPIR RAJASTHAN""")   # details Edit according to you 
+server.sendmail('sender mail address','receiver mail address', 
+
+
+
+                " BODY OF MAIL "
+                
+                
+                )   # details Edit according to you 
 server.quit()
